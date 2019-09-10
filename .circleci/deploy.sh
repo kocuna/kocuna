f8e5e66ec867ab33e363bc40b11dc0e8655d37e9
kocuna <54960562+kocuna@users.noreply.github.com>
2019-09-10 14:25:13 -0700

    fix (#11)

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,5 +2,5 @@
 
 
 kubectl apply -f k8s
-kubectl set image deployments/app-deployement app=kocuna/app:$CIRCLE_SHA1
-kubectl set image deployments/api-deployement api=kocuna/api:$CIRCLE_SHA1
+kubectl set image deployments/app-deployment app=kocuna/app:$CIRCLE_SHA1
+kubectl set image deployments/api-deployment api=kocuna/api:$CIRCLE_SHA1
