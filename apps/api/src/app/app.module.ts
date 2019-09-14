3a9dad25e1b969bf6bb21f7f0d0efae51ff81e81
carlos <carlos@kocuna.com>
2019-09-14 14:50:31 -0700

    generate tasks module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  imports: [TasksModule],
+  providers: [AppService]
 })
 export class AppModule {}
